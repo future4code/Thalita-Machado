049d2eafdc4a1fbf433287737328da81307980c0
Thalita Machado <thalitamh5@gmail.com>
2020-07-27 16:57:15 -0300

    variaveis-concluidas

---
+++ semana2/aula9-variaveis/exercicio1/index.js
@@ -0,0 +1,18 @@
+/*const primeiroNome = "Thalita"
+const Sobrenome= "Machado"
+
+console.log(primeiroNome,Sobrenome)*/
+
+/*let souUmBoolean = true
+console.log(souUmBoolean)*/
+
+/*let greysAnatomy = "greysAnatomy"
+let temporadas ="16" 
+console.log(typeof(greysAnatomy))*/
+
+
+/*let variavel = undefined (garante o valor da variavel)
+console.log((variavel)) */ 
+
+/*let outraVariavel = null
+console.log(outraVariavel)*/
---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,58 @@
+/*const nome 
+
+console.log(typeof(nome))
+
+
+let idade 
+console.log(typeof(idade)) */ 
+
+
+// no console deu erro pois não coloquei o valor da variavel// 
+
+
+/*const nome = prompt("Qual o seu nome?")
+
+console.log(nome)
+
+let idade = prompt("Qual a sua idade?")
+
+console.log(idade) 
+
+//no console apareceu nome e idade digitados // 
+
+
+console.log = (typeof(nome)) 
+
+console.log = (typeof(idade))
+
+//no console aparece string para ambas // */
+
+
+
+/*let endereço = prompt("Qual é o seu endereço?")
+
+console.log(endereço)
+
+let cor = prompt("qual é sua cor favorita?") 
+
+console.log(cor)
+
+let bicho = prompt("Qual é sua bicho favorito?")
+
+console.log(bicho)
+
+let bairro = prompt("Qual é seu bairro favorito?")
+
+console.log(bairro)
+
+let cidade = prompt("Qual é sua cidade favorito?")
+
+console.log(cidade) 
+
+// no console apareceu todas respostas que foi digitada na caixa de msg //*/ 
+
+
+
+
+
+
